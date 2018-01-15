c62e4b7d398f0d8b20753782bf60a13c8631d305
Terry Cheng <terrycheng@terrys-mbp.lan>
2018-01-14 21:42:41 -0800

    add binary search

--- Swift Tutorial/Control_Flow.playground/Contents.swift
+++ Swift Tutorial/Control_Flow.playground/Contents.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-// for-in loop
+//for-in loop
 //可用 "a...b" 來代表 a 到 b 的範圍(含b)，而 "a..<b" 代表 a 到 b-1 的範圍(不含b)
 for index in 1...5 {
     print("\(index) time 5 is \(index * 5)")
@@ -12,9 +12,17 @@ for index in 1..<5 {
     print("\(index) time 5 is \(index * 5)")
 }
 
-//如果不需要用到 index，可以用 "_" 來忽略。
+//如果不需要用到 index，可以用 "_" 來忽略
 for _ in 1...5 {
     print("Hello World")
 }
 
-// while loop
+//while loop
+//while 在每次 loop 開始前檢查條件
+var index = 1
+while index <= 5 {
+    print("\(index) time 5 is \(index * 5)")
+    index += 1
+}
+
+
